27bd8696234599dcb18274bfbc31fd29a89d5b8b
Nilaco <nicolazzara@gmail.com>
2015-07-10 17:07:23 -0700

    Add grandparent association and populate family tree

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Hold family members
 var familyTree = [];
 
 function Person(name)
@@ -6,23 +7,49 @@ function Person(name)
 	this.parent = null;
 	this.children = [];
 	this.siblings = [];
+	this.grandparent = null;
 	
-	this.addChild = function(name) 
-	{
-		var child = new Person(name)
-		child.parent = this
+	this.addChild = function(name) {
+		var child = new Person(name);
+		child.parent = this;
+			if (child.parent.parent != null) {
+				child.grandparent = child.parent.parent;
+			}
 		this.children.push(child);
 	};
-	
 	this.init();
 };
 
+//Add new people to family tree
 Person.prototype.init = function(){
 	familyTree.push(this)
 };
 
-//Driver test
-console.log(familyTree);
+var findPerson = function(name){
+	for (var i = 0; i < familyTree.length; i++) {
+		if (familyTree[i].name === name){
+//			console.log(familyTree[i]);
+			return familyTree[i];
+		}
+	}
+	console.log("Person not found");
+}
+
+//Add family members
 var nancy = new Person("Nancy");
 nancy.addChild("Adam")
-console.log(familyTree);
+nancy.addChild("Jill")
+nancy.addChild("Carl")
+findPerson("Carl").addChild("Joseph")
+findPerson("Carl").addChild("Catherine")
+findPerson("Jill").addChild("Kevin")
+findPerson("Kevin").addChild("Samuel")
+findPerson("Kevin").addChild("George")
+findPerson("Kevin").addChild("James")
+findPerson("Kevin").addChild("Aaron")
+findPerson("James").addChild("Mary")
+findPerson("George").addChild("Patrick")
+findPerson("George").addChild("Robert")
+
+
+console.log(findPerson("James").grandparent)
